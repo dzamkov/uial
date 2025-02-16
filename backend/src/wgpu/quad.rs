239fbfc8440dd09f7f6570e72b56852b293881fd
Dmitry Zamkov <dmitry94@gmail.com>
2025-02-15 18:32:35 -0600

    Loosening lifetime constraint of `draw_quad`

--- backend/src/wgpu/quad.rs
+++ backend/src/wgpu/quad.rs
@@ -2,7 +2,7 @@ use wgpu::util::DeviceExt;
 
 /// Encapsulates the resources needed to draw a simple unit quad in the bounds ([0, 1], [0, 1]).
 pub struct QuadResources {
-    vertex_buffer: wgpu::Buffer
+    vertex_buffer: wgpu::Buffer,
 }
 
 impl QuadResources {
@@ -14,36 +14,32 @@ impl QuadResources {
             contents: bytemuck::cast_slice(&vertices),
             usage: wgpu::BufferUsages::VERTEX,
         });
-        Self {
-            vertex_buffer
-        }
+        Self { vertex_buffer }
     }
 
     /// The vertex buffer layout for the quad.
-    pub const BUFFERS: &'static [wgpu::VertexBufferLayout<'static>] = &[
-        wgpu::VertexBufferLayout {
-            array_stride: 8 as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[wgpu::VertexAttribute {
-                format: wgpu::VertexFormat::Float32x2,
-                offset: 0,
-                shader_location: 0,
-            }],
-        }
-    ];
+    pub const BUFFERS: &'static [wgpu::VertexBufferLayout<'static>] = &[wgpu::VertexBufferLayout {
+        array_stride: 8 as wgpu::BufferAddress,
+        step_mode: wgpu::VertexStepMode::Vertex,
+        attributes: &[wgpu::VertexAttribute {
+            format: wgpu::VertexFormat::Float32x2,
+            offset: 0,
+            shader_location: 0,
+        }],
+    }];
 
     /// The primitive topology for the quad.
     pub const PRIMITIVE_TOPOLOGY: wgpu::PrimitiveTopology = wgpu::PrimitiveTopology::TriangleStrip;
 }
 
 /// Contains quad-related [`wgpu::RenderPass`] extension methods.
-pub trait RenderPassExt<'a> {
+pub trait RenderPassExt {
     /// Draws a unit quad. This automatically binds the appropriate geometry buffers.
-    fn draw_quad(&mut self, resources: &'a QuadResources);
+    fn draw_quad(&mut self, resources: &QuadResources);
 }
 
-impl<'a> RenderPassExt<'a> for wgpu::RenderPass<'a> {
-    fn draw_quad(&mut self, resources: &'a QuadResources) {
+impl RenderPassExt for wgpu::RenderPass<'_> {
+    fn draw_quad(&mut self, resources: &QuadResources) {
         self.set_vertex_buffer(0, resources.vertex_buffer.slice(..));
         self.draw(0..4, 0..1);
     }
